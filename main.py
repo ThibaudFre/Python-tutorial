21e18c8660ff3f732f9fc8270f7bc828de1cdcea
Thibaud Fresnay <thibaudfresnay.pro@gmail.com>
2024-12-11 14:49:19 +0100

    Annotations part started

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-#Docstrings
+#Annotations
 
-def myFunc (value):
-    """this is the documentation about my func it should return "my result" + the result"""
-    return ("my result " + value)
-
-print(help(myFunc))
