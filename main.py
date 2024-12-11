b002e21601638999b12676eb58d0dd2c3435781a
Thibaud Fresnay <thibaudfresnay.pro@gmail.com>
2024-12-11 14:48:22 +0100

    Docstrings finished

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-#Decorators
+#Docstrings
 
-def logtime(func):
-    def wrapper():
-        print("before")
-        val = func()
-        print("after")
-        return val
-    return wrapper
+def myFunc (value):
+    """this is the documentation about my func it should return "my result" + the result"""
+    return ("my result " + value)
 
-@logtime
-def hello():
-    print("hello");
-
-hello()
+print(help(myFunc))
