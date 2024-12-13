e5b52bc9a801ec4c091a9b0e53ecda9a89a46ec9
Thibaud Fresnay <thibaudfresnay.pro@gmail.com>
2024-12-13 15:24:22 +0100

    Operator overloading part finished let's practice with BalckJack !

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#Polymorphism
+#Operator Overloading
+
+class Dog:
+    # the Dog class
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __gt__(self, other):
+        return True if self.age > other.age else False
+
+roger = Dog("Roger", 8)
+syd = Dog("syd", 9)
+
+print(roger > syd)
