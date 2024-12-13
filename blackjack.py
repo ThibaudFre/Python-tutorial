aad8b09392d658f74806d26e0406681fb1b35f16
Thibaud Fresnay <thibaudfresnay.pro@gmail.com>
2024-12-13 18:13:43 +0100

    starter for black jack game, variables initialised loop to create carts done

---
+++ blackjack.py
@@ -0,0 +1,9 @@
+suits = ["hearts" , "clubs", "spades", "diamonds"]
+ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+cards = []
+
+for suit in suits :
+    for rank in ranks:
+        cards.append([suit, rank])
+
+print(len(cards))
