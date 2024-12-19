92eb0525eb5a195cd7257ce34f704eb3c6e9a760
Thibaud <thibaudfresnay.pro@gmail.com>
2024-12-19 14:49:08 +0100

    shuffle instancied, if else statements added to determine the value

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ["hearts" , "clubs", "spades", "diamonds"]
 ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 cards = []
@@ -6,4 +8,26 @@ for suit in suits :
     for rank in ranks:
         cards.append([suit, rank])
 
-print(len(cards))
+def shuffle():
+    random.shuffle(cards)
+
+def deal(number):
+    cards_dealt = []
+    for x in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+shuffle()
+cards_dealt = deal(2)
+card = cards_dealt[0]
+rank = cards[1]
+print(card)
+
+if rank == "A" :
+    value = 11
+elif rank == "J" or rank == "Q" or rank == "K":
+    value = 10
+
+print(card)
+    
