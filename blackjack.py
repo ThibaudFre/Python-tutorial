662ae4880e5d720463b9a5bfce78ba6a92b92cd7
Thibaud <thibaudfresnay.pro@gmail.com>
2024-12-26 11:34:17 +0100

    Deck class and hand Class have been created and a test has been done

--- blackjack.py
+++ blackjack.py
@@ -1,36 +1,60 @@
 import random
 
-suits = ["hearts" , "clubs", "spades", "diamonds"]
-ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-cards = []
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank["rank"]} of {self.suit}"
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["hearts" , "clubs", "spades", "diamonds"]
+        ranks = [
+            {"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value": 5},
+            {"rank": "6", "value": 6},
+            {"rank": "7", "value": 7},
+            {"rank": "8", "value": 8},
+            {"rank": "9", "value": 9},
+            {"rank": "10", "value": 10},
+            {"rank": "J", "value": 10},
+            {"rank": "Q", "value": 10},
+            {"rank": "K", "value": 10}
+        ]
 
-for suit in suits :
-    for rank in ranks:
-        cards.append([suit, rank])
+        for suit in suits :
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
 
-def shuffle():
-    random.shuffle(cards)
+    def shuffle(self):
+        if len(self.cards) < 1:
+            random.shuffle(self.cards)
 
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
 
-shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
 
-if rank == "A" :
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
 
-rank_dict = {"rank": rank, "value": value}
+deck = Deck()
+deck.shuffle()
 
-print(rank_dict["rank"], rank_dict["value"])
-    
+hand = Hand()
+hand.add_card(deck.deal(2))
+
+print(hand.cards)
